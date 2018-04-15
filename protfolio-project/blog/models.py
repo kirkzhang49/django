7c30ca8169fcfdd509d1b45271e04fdc808c72b6
kirkzhang49 <314698267@qq.com>
2018-04-14 19:55:27 -0700

    change for database and model

--- protfolio-project/blog/models.py
+++ protfolio-project/blog/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 # Create your models here.
 class blog(models.Model):
+    title = models.CharField(max_length = 100)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
